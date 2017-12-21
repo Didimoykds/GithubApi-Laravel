d25b56bd75b0a6737f1a7acea9b2e43a2ca2c9c9
Didimoykds <didimoykds@gmail.com>
2017-12-21 01:25:44 -0200

    Changed Welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,8 @@
             Welcome!
         </div>
         <a class="just_phrase" href="https://laravel-news.com/category/laravel-5.5"> Get updated to Laravel 5.5!</a>
-        <p>I know how to develop on Laravel 5.4 too! </p>
         <div class="links">
-            <a href="{{route('get_github')}}">Github Challenge!</a>
+            <a href="{{route('get_github')}}">Consult Github</a>
         </div>
     </div>
 </div>
